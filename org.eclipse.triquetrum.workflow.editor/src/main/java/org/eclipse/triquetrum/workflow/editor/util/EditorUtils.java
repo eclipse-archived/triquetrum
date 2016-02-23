14acc8a0e05bba7686884a97fb4e00b4b3cb18c2
Erwin De Ley <erwindl0@gmail.com>
2016-02-23 23:00:43 +0100

    resolve NPE when opening colordialog in a fresh diagram
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/util/EditorUtils.java
+++ org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/util/EditorUtils.java
@@ -27,12 +27,13 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.RGBA;
 import org.eclipse.swt.widgets.ColorDialog;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.triquetrum.workflow.editor.TriqDiagramEditor;
 import org.eclipse.triquetrum.workflow.model.CompositeActor;
 import org.eclipse.triquetrum.workflow.model.CompositeEntity;
 import org.eclipse.triquetrum.workflow.model.NamedObj;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
 
 public class EditorUtils {
 
@@ -40,18 +41,17 @@ public class EditorUtils {
    * Opens a dialog to change the color.
    *
    * @param color
-   *            the color to change
+   *          the color to change
    * @return the changed color
    */
   public static String editColor(Diagram diagram, String colorStr) {
     Color color = null;
-    if(colorStr==null) {
+    if (colorStr == null) {
       color = Graphiti.getGaService().manageColor(diagram, IColorConstant.BLACK);
     } else {
       color = EditorUtils.buildColorFromString(diagram, colorStr);
     }
-    Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-    ColorDialog colorDialog = new ColorDialog(shell);
+    ColorDialog colorDialog = new ColorDialog(Display.getDefault().getActiveShell());
     colorDialog.setText("Choose color");
     colorDialog.setRGB(new RGB(color.getRed(), color.getGreen(), color.getBlue()));
 
@@ -64,38 +64,37 @@ public class EditorUtils {
   }
 
   /**
-   * Current color-treatment is based on storing a comma-separated string of rgba values.
-   * RGBA values must be integers in the range [0-255].
+   * Current color-treatment is based on storing a comma-separated string of rgba values. RGBA values must be integers in the range [0-255].
    * <p>
    * Each time the actual color is needed, this must be parsed and converted to a Color instance.
    *
    * TODO : add Color as an EMF model entity, and figure out how EMF Forms can integrate a color picker control that supports data binding etc.
    * </p>
-   * Remark that Graphiti's Color does not support setting opacity/alpha.
-   * So its value is currently silently ignored in Triq.
+   * Remark that Graphiti's Color does not support setting opacity/alpha. So its value is currently silently ignored in Triq.
    *
-   * @param colorStr a comma-separated rgba value list. It can be partially specified, e.g. just rg or so.
-   *  The non-specified elements will be set with defaults of 0 for the color components and 1 for the alpha.
+   * @param colorStr
+   *          a comma-separated rgba value list. It can be partially specified, e.g. just rg or so. The non-specified elements will be set with defaults of 0
+   *          for the color components and 1 for the alpha.
    * @return
    *
    * @see RGBA
    * @see Color
    */
   public static Color buildColorFromString(Diagram diagram, String colorStr) {
-    String[] colorParts = colorStr!=null ? colorStr.split(",") : new String[]{};
+    String[] colorParts = colorStr != null ? colorStr.split(",") : new String[] {};
     int red = 0;
     int green = 0;
     int blue = 0;
     // alpha is not supported by graphiti colors it seems...
-//    int alpha = 1;
+    // int alpha = 1;
 
-    if(colorParts.length>0) {
+    if (colorParts.length > 0) {
       red = Integer.parseInt(colorParts[0]);
     }
-    if(colorParts.length>1) {
+    if (colorParts.length > 1) {
       green = Integer.parseInt(colorParts[1]);
     }
-    if(colorParts.length>2) {
+    if (colorParts.length > 2) {
       blue = Integer.parseInt(colorParts[2]);
     }
 
@@ -107,7 +106,7 @@ public class EditorUtils {
    * @return a ","-separated list of the color's rgb values followed by a fixed 255 alpha value
    */
   public static String toString(Color color) {
-    return color.getRed()+","+color.getGreen()+","+color.getBlue()+",255";
+    return color.getRed() + "," + color.getGreen() + "," + color.getBlue() + ",255";
   }
 
   /**
@@ -115,61 +114,64 @@ public class EditorUtils {
    * @return a ","-separated list of the color's rgb values followed by a fixed 255 alpha value
    */
   public static String toString(RGB color) {
-    return color.red+","+color.green+","+color.blue+",255";
+    return color.red + "," + color.green + "," + color.blue + ",255";
   }
 
   /**
-   * Creates a unique name for a new model element, in its container.
-   * The relatedObj should be either the container or a sibling of the to-be-created element,
+   * Creates a unique name for a new model element, in its container. The relatedObj should be either the container or a sibling of the to-be-created element,
    * i.e. it should be in the same container as the new element.
    * <p>
    * This method delegates to ptolemy.kernel.CompositeEntity.uniqueName(String)
    * </p>
-   * @param relatedObj a model element in the same container as the new element, or the container itself.
-   * @param prefix a prefix for the generated unique name.
+   *
+   * @param relatedObj
+   *          a model element in the same container as the new element, or the container itself.
+   * @param prefix
+   *          a prefix for the generated unique name.
    * @return a unique name constructed from the prefix.
    */
   public static String buildUniqueName(NamedObj relatedObj, String prefix) {
     NamedObj container = relatedObj;
-    while((container != null) && !(container instanceof CompositeEntity)) {
+    while ((container != null) && !(container instanceof CompositeEntity)) {
       container = container.getContainer();
     }
-    if(container==null) {
+    if (container == null) {
       return prefix;
     } else {
-      return ((ptolemy.kernel.CompositeEntity)container.getWrappedObject()).uniqueName(prefix);
+      return ((ptolemy.kernel.CompositeEntity) container.getWrappedObject()).uniqueName(prefix);
     }
   }
 
   /**
    * Find the diagram for the selected editor part.
+   *
    * @return the diagram from the selected diagram editor
    */
   @SuppressWarnings("restriction")
   public static Diagram getSelectedDiagram() {
     Diagram result = null;
     IWorkbenchPage page = EclipseUtils.getPage();
-    final ISelection sel = page!=null ? page.getSelection() : null;
-    if (sel != null && sel instanceof IStructuredSelection) {
-      final IStructuredSelection str = (IStructuredSelection) sel;
-      Object res = str.getFirstElement();
-      if (res instanceof DiagramEditPart) {
-        result = (Diagram) ((DiagramEditPart) res).getModel();
+    if (page != null) {
+      for (IEditorReference editorRef : page.getEditorReferences()) {
+        if (editorRef.getId().contains("triquetrum")) {
+          result = ((TriqDiagramEditor) editorRef.getEditor(true)).getDiagramTypeProvider().getDiagram();
+          break;
+        }
       }
     }
     return result;
   }
 
-
   /**
    * Find the model from the selected editor part and its contained diagram.
+   *
    * @return the model from the selected diagram editor
    */
   @SuppressWarnings("restriction")
   public static CompositeActor getSelectedModel() {
     NamedObj result = null;
     IWorkbenchPage page = EclipseUtils.getPage();
-    final ISelection sel = page!=null ? page.getSelection() : null;
+    final ISelection sel = page != null ? page.getSelection() : null;
     if (sel != null && sel instanceof IStructuredSelection) {
       final IStructuredSelection str = (IStructuredSelection) sel;
       Object res = str.getFirstElement();
@@ -190,6 +192,7 @@ public class EditorUtils {
 
   /**
    * Find the model element for the selected edit part
+   *
    * @param editPart
    * @return
    */
