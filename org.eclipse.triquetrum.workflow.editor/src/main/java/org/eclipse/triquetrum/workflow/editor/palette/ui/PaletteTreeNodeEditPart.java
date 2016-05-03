7e3ac7f30a6ed6edf9d5b9baac912fd6238c5340
Erwin De Ley <erwindl0@gmail.com>
2016-05-03 19:33:20 +0200

    #1: fix copyrights
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteTreeNodeEditPart.java
+++ org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteTreeNodeEditPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 iSencia Belgium NV.
+ * Copyright (c) 2015, 2016 iSencia Belgium NV.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,10 +16,6 @@ import java.util.List;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.palette.PaletteContainer;
 import org.eclipse.gef.palette.PaletteEntry;
-import org.eclipse.graphiti.ui.services.GraphitiUi;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.triquetrum.workflow.editor.ImageConstants;
-import org.eclipse.triquetrum.workflow.editor.TriqDiagramTypeProvider;
 
 /**
  * Tree EditPart for palette tree node.
