dfca3ed51d7485b21fdfa505c775c5aea202d6d3
Erwin De Ley <erwindl0@gmail.com>
2016-05-11 14:54:51 +0200

    #1: adapt bold font generation for palette tree nodes
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteLabelProvider.java
+++ org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteLabelProvider.java
@@ -10,22 +10,31 @@
  *******************************************************************************/
 package org.eclipse.triquetrum.workflow.editor.palette.ui;
 
+import org.eclipse.jface.resource.FontDescriptor;
 import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.IFontProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.widgets.Widget;
 
-public class PaletteLabelProvider implements ILabelProvider, IColorProvider {
+public class PaletteLabelProvider implements ILabelProvider, IColorProvider, IFontProvider {
+  private static Font BOLD_FONT;
+  private PaletteTreeViewer viewer;
 
   /**
    * Constructor
    *
    * @param viewer
-   *            The TreeViewer for which this instance is a LabelProvider
+   *          The TreeViewer for which this instance is a LabelProvider
    */
-  public PaletteLabelProvider(TreeViewer viewer) {
+  public PaletteLabelProvider(PaletteTreeViewer viewer) {
+    this.viewer = viewer;
   }
 
   /**
@@ -72,8 +81,7 @@ public class PaletteLabelProvider implements ILabelProvider, IColorProvider {
   }
 
   /**
-   * @see org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(Object,
-   *      String)
+   * @see org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(Object, String)
    */
   public boolean isLabelProperty(Object element, String property) {
     return false;
@@ -85,6 +93,30 @@ public class PaletteLabelProvider implements ILabelProvider, IColorProvider {
    * @see org.eclipse.jface.viewers.IBaseLabelProvider#removeListener(ILabelProviderListener)
    */
   public void removeListener(ILabelProviderListener listener) {
+  }
+
+  @Override
+  public Font getFont(Object element) {
+    if (element instanceof PaletteTreeNodeEditPart) {
+      if (BOLD_FONT == null) {
+        Widget widget = ((PaletteEntryEditPart) element).getWidget();
+        if (widget instanceof TreeItem) {
+          TreeItem treeItem = (TreeItem) widget;
+          Font f = null;
+          // there's something about the order of tree construction logic that causes treeitems to not be
+          // fully ready yet here
+          if (!treeItem.isDisposed()) {
+            f = treeItem.getFont();
+          } else {
+            f = new Font(Display.getCurrent(), viewer.getPaletteViewerPreferences().getFontData());
+          }
+          FontDescriptor boldDescriptor = FontDescriptor.createFrom(f).setStyle(SWT.BOLD);
+          BOLD_FONT = boldDescriptor.createFont(Display.getCurrent());
+        }
+      }
+      return BOLD_FONT;
+    }
+    return null;
   }
 
 }
--- org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteTreeViewer.java
+++ org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteTreeViewer.java
@@ -96,7 +96,7 @@ public class PaletteTreeViewer extends PaletteViewer {
     FilteredTree tree = new FilteredTree(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL, filter, true);
     tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
     tree.getViewer().setContentProvider(new PaletteTreeProvider());
-    tree.getViewer().setLabelProvider(new PaletteLabelProvider(tree.getViewer()));
+    tree.getViewer().setLabelProvider(new PaletteLabelProvider(this));
     setControl(tree);
     return tree;
   }
