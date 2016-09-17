35ec5bee73cb55cec11ba16fefbdb519bdc1919f
Erwin De Ley <erwindl0@gmail.com>
2016-09-17 21:07:05 +0200

    #2: first functional prototype of plain submodels
    - add/remove ports on a composite via the actor config dialog
    - ports stick to the left/right sides similarly as for atomic actors
    - composite actors can be expanded/collapsed and can be resized when
    expanded
    - drop actors, parameters, vertex-es (vertices?) ... on/in a composite
    
    It's all not too polished yet, but most basic features seem to work ;-)
    
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/features/ModelElementResizeFeature.java
+++ org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/features/ModelElementResizeFeature.java
@@ -11,7 +11,12 @@
 package org.eclipse.triquetrum.workflow.editor.features;
 
 import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.IResizeConfiguration;
+import org.eclipse.graphiti.features.context.IResizeShapeContext;
 import org.eclipse.graphiti.features.impl.DefaultResizeShapeFeature;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.triquetrum.workflow.editor.BoCategory;
+import org.eclipse.triquetrum.workflow.editor.util.EditorUtils;
 
 public class ModelElementResizeFeature extends DefaultResizeShapeFeature {
 
@@ -19,9 +24,27 @@ public class ModelElementResizeFeature extends DefaultResizeShapeFeature {
     super(fp);
   }
 
-  // @Override
-  // public IResizeConfiguration getResizeConfiguration(IResizeShapeContext context) {
-  // return new NoResizeConfiguration();
-  // }
+  @Override
+  public IResizeConfiguration getResizeConfiguration(IResizeShapeContext context) {
+    PictogramElement pe = context.getPictogramElement();
+    BoCategory boCategory = BoCategory.retrieveFrom(pe);
+    if (BoCategory.CompositeActor.equals(boCategory) && !EditorUtils.isCollapsed(pe)) {
+      return super.getResizeConfiguration(context);
+    } else {
+      return new NoResizeConfiguration();
+    }
+  }
+
+  private static class NoResizeConfiguration implements IResizeConfiguration {
+    @Override
+    public boolean isVerticalResizeAllowed() {
+      return false;
+    }
+
+    @Override
+    public boolean isHorizontalResizeAllowed() {
+      return false;
+    }
+  }
 
 }
