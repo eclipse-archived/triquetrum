25c9b6208632d0a30fd1e73994a7a0d26cdf8348
Erwin De Ley <erwindl0@gmail.com>
2017-07-05 20:06:23 +0200

    #223: support drawing relations from input to output as well
    i.o. previously always going from output to input
    
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- plugins/editor/org.eclipse.triquetrum.workflow.model/src/main/java/org/eclipse/triquetrum/workflow/model/impl/PortImpl.java
+++ plugins/editor/org.eclipse.triquetrum.workflow.model/src/main/java/org/eclipse/triquetrum/workflow/model/impl/PortImpl.java
@@ -49,8 +49,10 @@ import ptolemy.kernel.util.IllegalActionException;
  * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#isMultiPort <em>Multi Port</em>}</li>
  * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#getDirection <em>Direction</em>}</li>
  * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#getLinkedRelations <em>Linked Relations</em>}</li>
- * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#getInsideLinkedRelations <em>Inside Linked Relations</em>}</li>
- * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#getOutsideLinkedRelations <em>Outside Linked Relations</em>}</li>
+ * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#getInsideLinkedRelations <em>Inside Linked
+ * Relations</em>}</li>
+ * <li>{@link org.eclipse.triquetrum.workflow.model.impl.PortImpl#getOutsideLinkedRelations <em>Outside Linked
+ * Relations</em>}</li>
  * </ul>
  *
  * @generated
@@ -75,7 +77,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected boolean input = INPUT_EDEFAULT;
 
   /**
-   * The default value of the '{@link #isOutput() <em>Output</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The default value of the '{@link #isOutput() <em>Output</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+   * -->
    * 
    * @see #isOutput()
    * @generated
@@ -84,7 +87,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected static final boolean OUTPUT_EDEFAULT = false;
 
   /**
-   * The cached value of the '{@link #isOutput() <em>Output</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The cached value of the '{@link #isOutput() <em>Output</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+   * -->
    * 
    * @see #isOutput()
    * @generated
@@ -93,7 +97,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected boolean output = OUTPUT_EDEFAULT;
 
   /**
-   * The default value of the '{@link #isMultiPort() <em>Multi Port</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The default value of the '{@link #isMultiPort() <em>Multi Port</em>}' attribute. <!-- begin-user-doc --> <!--
+   * end-user-doc -->
    * 
    * @see #isMultiPort()
    * @generated
@@ -102,7 +107,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected static final boolean MULTI_PORT_EDEFAULT = false;
 
   /**
-   * The cached value of the '{@link #isMultiPort() <em>Multi Port</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The cached value of the '{@link #isMultiPort() <em>Multi Port</em>}' attribute. <!-- begin-user-doc --> <!--
+   * end-user-doc -->
    * 
    * @see #isMultiPort()
    * @generated
@@ -111,7 +117,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected boolean multiPort = MULTI_PORT_EDEFAULT;
 
   /**
-   * The default value of the '{@link #getDirection() <em>Direction</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The default value of the '{@link #getDirection() <em>Direction</em>}' attribute. <!-- begin-user-doc --> <!--
+   * end-user-doc -->
    * 
    * @see #getDirection()
    * @generated
@@ -120,7 +127,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected static final Direction DIRECTION_EDEFAULT = Direction.DEFAULT;
 
   /**
-   * The cached value of the '{@link #getDirection() <em>Direction</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The cached value of the '{@link #getDirection() <em>Direction</em>}' attribute. <!-- begin-user-doc --> <!--
+   * end-user-doc -->
    * 
    * @see #getDirection()
    * @generated
@@ -137,7 +145,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected boolean directionESet;
 
   /**
-   * The cached value of the '{@link #getLinkedRelations() <em>Linked Relations</em>}' reference list. <!-- begin-user-doc --> <!-- end-user-doc -->
+   * The cached value of the '{@link #getLinkedRelations() <em>Linked Relations</em>}' reference list. <!-- begin-user-doc
+   * --> <!-- end-user-doc -->
    * 
    * @see #getLinkedRelations()
    * @generated
@@ -146,8 +155,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected EList<Relation> linkedRelations;
 
   /**
-   * The cached value of the '{@link #getInsideLinkedRelations() <em>Inside Linked Relations</em>}' reference list. <!-- begin-user-doc --> <!-- end-user-doc
-   * -->
+   * The cached value of the '{@link #getInsideLinkedRelations() <em>Inside Linked Relations</em>}' reference list. <!--
+   * begin-user-doc --> <!-- end-user-doc -->
    *
    * @see #getInsideLinkedRelations()
    * @generated
@@ -156,8 +165,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   protected EList<Relation> insideLinkedRelations;
 
   /**
-   * The cached value of the '{@link #getOutsideLinkedRelations() <em>Outside Linked Relations</em>}' reference list. <!-- begin-user-doc --> <!-- end-user-doc
-   * -->
+   * The cached value of the '{@link #getOutsideLinkedRelations() <em>Outside Linked Relations</em>}' reference list. <!--
+   * begin-user-doc --> <!-- end-user-doc -->
    *
    * @see #getOutsideLinkedRelations()
    * @generated
@@ -311,8 +320,8 @@ public class PortImpl extends NamedObjImpl implements Port {
   }
 
   /**
-   * <!-- begin-user-doc --> Checks whether the port can accept a new outside relation connection. For single ports, this means that if there is already a
-   * connection, the new request must be refused. <!-- end-user-doc -->
+   * <!-- begin-user-doc --> Checks whether the port can accept a new outside relation connection. For single ports, this
+   * means that if there is already a connection, the new request must be refused. <!-- end-user-doc -->
    *
    * @generated NOT
    */
@@ -322,8 +331,9 @@ public class PortImpl extends NamedObjImpl implements Port {
   }
 
   /**
-   * <!-- begin-user-doc --> Checks whether the port can accept a new inside relation connection. For single ports, this means that if there is already an
-   * inside relation, the new request must be refused. The container must be a CompositeActor, to allow inside relations at all. <!-- end-user-doc -->
+   * <!-- begin-user-doc --> Checks whether the port can accept a new inside relation connection. For single ports, this
+   * means that if there is already an inside relation, the new request must be refused. The container must be a
+   * CompositeActor, to allow inside relations at all. <!-- end-user-doc -->
    *
    * @generated NOT
    */
@@ -353,15 +363,17 @@ public class PortImpl extends NamedObjImpl implements Port {
   }
 
   /**
-   * <!-- begin-user-doc --> This method checks if this port can be the src for a new connection. For ports, this is true when the port is an atomic actor's
-   * output port, or is a port inside a submodel CompositeActor.
-   *
+   * <!-- begin-user-doc --> This method checks if this port can be the src for a new connection. For ports, this is true
+   * when the port has remaining capacity to start either a new outside or new inside relation. Remark that connections
+   * can be drawn starting from an output port and ending in an input port as well, similarly to what Ptolemy's Vergil
+   * allows.
+   * 
    * @return true if the src can be a starting point for a new connection <!-- end-user-doc -->
    * @generated NOT
    */
   @Override
   public boolean isPotentialStart() {
-    return (isOutput() && canAcceptNewOutsideRelation()) || (isInput() && canAcceptNewInsideRelation());
+    return canAcceptNewOutsideRelation() || canAcceptNewInsideRelation();
   }
 
   /**
@@ -371,31 +383,43 @@ public class PortImpl extends NamedObjImpl implements Port {
    */
   @Override
   public boolean isPotentialEnd(Linkable start) {
-    // Output ports can only be a valid target when their container is a CompositeActor,
-    // and the source is an input port or vertex within the same composite or an output port of an actor within that composite.
-    if (isOutput() && canAcceptNewInsideRelation()) {
-      NamedObj targetContainerModelLevel = getContainer();
-      boolean isCorrectContainer = targetContainerModelLevel instanceof CompositeActor;
-      if (isCorrectContainer) {
-        boolean isSrcValidInSameComposite = targetContainerModelLevel.equals(start.getContainer()) && ((start instanceof Port) && ((Port) start).isInput());
-        boolean isSrcValidInActorInSameComposite = targetContainerModelLevel.equals(start.getContainer().getContainer())
-            && ((start instanceof Vertex) || ((start instanceof Port) && ((Port) start).isOutput()));
-        return isSrcValidInActorInSameComposite || isSrcValidInSameComposite;
-      } else {
-        return false;
+    boolean result = false;
+    if (isOutput()) {
+      // Output ports can be a valid target when their container is a CompositeActor, and the source is an input port
+      // or vertex within the same composite or an output port of an actor within that composite.
+      if (canAcceptNewInsideRelation()) {
+        NamedObj targetContainerModelLevel = getContainer();
+        boolean isCorrectContainer = targetContainerModelLevel instanceof CompositeActor;
+        if (isCorrectContainer) {
+          boolean isSrcValidInSameComposite = targetContainerModelLevel.equals(start.getContainer()) && ((start instanceof Port) && ((Port) start).isInput());
+          boolean isSrcValidInActorInSameComposite = targetContainerModelLevel.equals(start.getContainer().getContainer())
+              && ((start instanceof Vertex) || ((start instanceof Port) && ((Port) start).isOutput()));
+          result = isSrcValidInActorInSameComposite || isSrcValidInSameComposite;
+        }
+      }
+      // But an output port can also accept a connection from an input port in the same "model level",
+      // when the user is drawing a connection in reverse direction.
+      if(!result && canAcceptNewOutsideRelation()) {
+        NamedObj targetContainerModelLevel = getContainer().getContainer();
+        boolean isSrcValidVertex = (start instanceof Vertex) && (targetContainerModelLevel.equals(start.getContainer().getContainer()));
+        boolean isSrcValidPort = (start instanceof Port) && (((Port) start).isInput() && targetContainerModelLevel.equals(start.getContainer().getContainer())
+            || ((Port) start).isOutput() && targetContainerModelLevel.equals(start.getContainer()));
+        result = isSrcValidVertex || isSrcValidPort;
       }
     }
-    if (isInput() && canAcceptNewOutsideRelation()) {
-      // Input ports and Vertex targets can only receive connections from sources that are in the same encompassing model level,
-      // i.e. in the container of the target's container.
+    if (!result && isInput()) {
+      // Input ports and Vertex targets can receive connections from sources that are in the same encompassing model
+      // level, i.e. in the container of the target's container.
       // For source ports we need to differentiate between plain actor output ports and input ports on a submodel CompositeActor.
-      NamedObj targetContainerModelLevel = getContainer().getContainer();
-      boolean isSrcValidVertex = (start instanceof Vertex) && (targetContainerModelLevel.equals(start.getContainer().getContainer()));
-      boolean isSrcValidPort = (start instanceof Port) && (((Port) start).isOutput() && targetContainerModelLevel.equals(start.getContainer().getContainer())
-          || ((Port) start).isInput() && targetContainerModelLevel.equals(start.getContainer()));
-      return isSrcValidVertex || isSrcValidPort;
+      if (canAcceptNewOutsideRelation()) {
+        NamedObj targetContainerModelLevel = getContainer().getContainer();
+        boolean isSrcValidVertex = (start instanceof Vertex) && (targetContainerModelLevel.equals(start.getContainer().getContainer()));
+        boolean isSrcValidPort = (start instanceof Port) && (((Port) start).isOutput() && targetContainerModelLevel.equals(start.getContainer().getContainer())
+            || ((Port) start).isInput() && targetContainerModelLevel.equals(start.getContainer()));
+        result = isSrcValidVertex || isSrcValidPort;
+      }
     }
-    return false;
+    return result;
   }
 
   /**
@@ -465,11 +489,11 @@ public class PortImpl extends NamedObjImpl implements Port {
    * @generated NOT
    */
   public Direction getDirection() {
-    if(direction!=null && Direction.DEFAULT!=direction) {
+    if (direction != null && Direction.DEFAULT != direction) {
       return direction;
-    } else if(isInput()) {
+    } else if (isInput()) {
       return Direction.WEST;
-    } else if(isOutput()) {
+    } else if (isOutput()) {
       return Direction.EAST;
     } else {
       return direction;
