b49e3291e58692cde58b39ad670da77d5a82e10d
Erwin De Ley <erwindl0@gmail.com>
2017-07-16 18:16:18 +0200

    #78: getting the edditor palette to refresh for userlibrary changes
    first working steps (for adding subfolders & deleting actors)
    
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- plugins/editor/org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteTreeProvider.java
+++ plugins/editor/org.eclipse.triquetrum.workflow.editor/src/main/java/org/eclipse/triquetrum/workflow/editor/palette/ui/PaletteTreeProvider.java
@@ -10,27 +10,42 @@
  *******************************************************************************/
 package org.eclipse.triquetrum.workflow.editor.palette.ui;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.List;
 
 import org.eclipse.gef.EditPart;
+import org.eclipse.gef.palette.PaletteContainer;
+import org.eclipse.gef.palette.PaletteEntry;
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 
+/**
+ * Based on org.eclipse.gef.ui.palette.customize.PaletteTreeProvider. (which is not visible for reuse.)
+ *
+ */
 public class PaletteTreeProvider implements ITreeContentProvider {
 
-  /**
-   * @see org.eclipse.jface.viewers.IContentProvider#dispose()
-   */
-  @Override
-  public void dispose() {
+  private TreeViewer viewer;
+  private PaletteTreeNodeEditPart root;
+  private PropertyChangeListener modelListener = new PropertyChangeListener() {
+    public void propertyChange(PropertyChangeEvent evt) {
+      handlePropertyChanged(evt);
+    }
+  };
+
+  public PaletteTreeProvider(TreeViewer treeviewer) {
+    this.viewer = treeviewer;
   }
 
-  /**
-   * If the given element does not have any children, this method should return <code>null</code>. This fixes the problem where a "+" sign is incorrectly placed
-   * next to an empty container in the tree.
-   *
-   * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(Object)
-   */
+  @Override
+  public void dispose() {
+    if (root != null) {
+      traverseModel((PaletteEntry) root.getModel(), false);
+    }
+  }
+
   @Override
   public Object[] getChildren(Object parentElement) {
     if (parentElement instanceof List) {
@@ -42,22 +57,20 @@ public class PaletteTreeProvider implements ITreeContentProvider {
         return children.toArray();
       }
     }
+    if (parentElement instanceof PaletteContainer) {
+      List children = ((PaletteContainer) parentElement).getChildren();
+      if (!children.isEmpty()) {
+        return children.toArray();
+      }
+    }
     return null;
   }
 
-  /**
-   * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(Object)
-   */
   @Override
   public boolean hasChildren(Object element) {
     return getChildren(element) != null;
   }
 
-  /**
-   * This method should not return <code>null</code>.
-   *
-   * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(Object)
-   */
   @Override
   public Object[] getElements(Object inputElement) {
     Object[] elements = getChildren(inputElement);
@@ -67,18 +80,56 @@ public class PaletteTreeProvider implements ITreeContentProvider {
     return elements;
   }
 
-  /**
-   * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(Object)
-   */
   @Override
   public Object getParent(Object element) {
     return ((EditPart) element).getParent();
   }
 
-  /**
-   * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(Viewer, Object, Object)
-   */
   @Override
   public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+    if (root != null)
+      traverseModel((PaletteEntry) root.getModel(), false);
+    if (newInput != null) {
+      root = (PaletteTreeNodeEditPart) newInput;
+      traverseModel((PaletteEntry) root.getModel(), true);
+    }
+  }
+
+  /**
+   * This method is invoked whenever there is any change in the model. It updates the viewer with the changes that were
+   * made to the model. Sub-classes may override this method to change or extend its functionality.
+   * 
+   * @param evt
+   *          The {@link PropertyChangeEvent} that was fired from the model
+   */
+  protected void handlePropertyChanged(PropertyChangeEvent evt) {
+    PaletteEntry entry = ((PaletteEntry) evt.getSource());
+    String property = evt.getPropertyName();
+    if (property.equals(PaletteEntry.PROPERTY_LABEL) || property.equals(PaletteEntry.PROPERTY_SMALL_ICON)) {
+      viewer.update(entry, null);
+    } else if (property.equals(PaletteEntry.PROPERTY_VISIBLE)) {
+      viewer.refresh(entry);
+    } else if (property.equals(PaletteContainer.PROPERTY_CHILDREN)) {
+      viewer.refresh();
+      List<PaletteEntry> oldChildren = (List<PaletteEntry>) evt.getOldValue();
+      for (PaletteEntry child : oldChildren) {
+        traverseModel(child, false);
+      }
+      traverseModel(entry, true);
+    }
+  }
+
+  private void traverseModel(PaletteEntry entry, boolean isHook) {
+    if (isHook) {
+      entry.addPropertyChangeListener(modelListener);
+    } else {
+      entry.removePropertyChangeListener(modelListener);
+    }
+    Object[] children = getChildren(entry);
+    if (children != null) {
+      for (int i = 0; i < children.length; i++) {
+        traverseModel((PaletteEntry) children[i], isHook);
+      }
+    }
   }
 }
