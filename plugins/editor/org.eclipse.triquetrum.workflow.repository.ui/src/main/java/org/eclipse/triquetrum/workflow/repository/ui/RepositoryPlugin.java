8d2adf0cc3504a9aaccdb7b987758f474c81aba0
Erwin De Ley <erwindl0@gmail.com>
2019-09-22 23:48:36 +0200

    #313: update copyright headers to EPL-2.0
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- plugins/editor/org.eclipse.triquetrum.workflow.repository.ui/src/main/java/org/eclipse/triquetrum/workflow/repository/ui/RepositoryPlugin.java
+++ plugins/editor/org.eclipse.triquetrum.workflow.repository.ui/src/main/java/org/eclipse/triquetrum/workflow/repository/ui/RepositoryPlugin.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2017,2019 iSencia Belgium NV.
+ *  
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Erwin De Ley - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.triquetrum.workflow.repository.ui;
 
 import org.eclipse.jface.resource.ImageDescriptor;
--- tests/org.eclipse.triquetrum.scisoft.analysis.rpc.test/src/org/eclipse/triquetrum/scisoft/analysis/rpc/test/StreamGobbler.java
+++ tests/org.eclipse.triquetrum.scisoft.analysis.rpc.test/src/org/eclipse/triquetrum/scisoft/analysis/rpc/test/StreamGobbler.java
@@ -1,13 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2016 Kichwa Coders
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Jonah Graham (Kichwa Coders) - initial API and implementation
+ *  Copyright (c) 2012-2016 Diamond Light Source Ltd.,
+ *                           Kichwa Coders & iSencia Belgium NV.
+ *                           
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License 2.0 which is available at
+ *  http://www.eclipse.org/legal/epl-2.0.
+ *  
+ *  SPDX-License-Identifier: EPL-2.0
+ *  
+ *  Contributors:
+ *      DLS, Kichwa Coders - initial API and implementation and/or initial documentation
+ *      Erwin De Ley - extraction from DAWN to ease reuse in other contexts
  *******************************************************************************/
 package org.eclipse.triquetrum.scisoft.analysis.rpc.test;
 
